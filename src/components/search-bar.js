d1cadcf0f250cbbae80e60bb737de2e7956e0b68
codingkapoor <mailtoshivamk@gmail.com>
2017-11-12 15:54:24 +0530

    search migrated to form based search

--- src/components/search-bar.js
+++ src/components/search-bar.js
@@ -1,27 +1,36 @@
 import React, { Component } from 'react';
-import { Input } from 'reactstrap';
+import { Form, FormGroup, Input, Button } from 'reactstrap';
 
 class SearchBar extends Component {
   constructor(props) {
     super(props);
     this.state = { term: '' };
+    this.onFormSubmit = this.onFormSubmit.bind(this);
   }
 
   render() {
     return (
       <div className = "search-bar col-md-6">
-        <Input
-          placeholder = "Search projects by technologies"
-          value = { this.state.term }
-          onChange = { event => this.onInputChange(event.target.value) } />
+        <Form onSubmit = { this.onFormSubmit }>
+          <FormGroup>
+            <Input
+              placeholder = "Search projects by technologies"
+              value = { this.state.term }
+              onChange = { event => this.setState({ term: event.target.value }) } />
+          </FormGroup>
+          <Button color="primary">
+            <i className="fa fa-search" aria-hidden="true"></i>
+          </Button>
+        </Form>
       </div>
     );
   }
 
-  onInputChange(term) {
-    this.setState({ term });
-    this.props.onSearchTermChange(term);
+  onFormSubmit(event) {
+    event.preventDefault();
+    this.props.onSearchTermChange(this.state.term);
   }
+
 }
 
 export default SearchBar;
