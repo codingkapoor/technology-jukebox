7b08c0c374d69dfab980040488e697d3c378a925
codingkapoor <mailtoshivamk@gmail.com>
2017-11-13 15:58:46 +0530

    autosearch intro

--- src/components/search-bar.js
+++ src/components/search-bar.js
@@ -1,22 +1,105 @@
 import React, { Component } from 'react';
 import { Form, FormGroup, Input, Button } from 'reactstrap';
+import AutoSuggest from 'react-autosuggest';
+
+const languages = [
+  {name: "Akka"},
+  {name: "BootStrap"},
+  {name: "CSS3"},
+  {name: "ES6"},
+  {name: "Grunt"},
+  {name: "HTML5"},
+  {name: "Java"},
+  {name: "JavaScript"},
+  {name: "JQuery"},
+  {name: "NPM"},
+  {name: "React"},
+  {name: "Redux"},
+  {name: "Resteasy"},
+  {name: "RequireJS"},
+  {name: "SASS"},
+  {name: "Scala"},
+  {name: "SigmaJS"},
+  {name: "Spring"},
+  {name: "Spring Boot"}
+];
+
+function escapeRegexCharacters(str) {
+  return str.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
+}
+
+function getSuggestions(value) {
+  const escapedValue = escapeRegexCharacters(value.trim());
+
+  if (escapedValue === '') {
+    return [];
+  }
+
+  const regex = new RegExp('^' + escapedValue, 'i');
+
+  return languages.filter(language => regex.test(language.name));
+}
+
+function getSuggestionValue(suggestion) {
+  return suggestion.name;
+}
+
+function renderSuggestion(suggestion) {
+  return (
+    <span>{suggestion.name}</span>
+  );
+}
 
 class SearchBar extends Component {
   constructor(props) {
     super(props);
-    this.state = { term: '' };
+
+    this.state = {
+      value: '',
+      suggestions: []
+    };
+
     this.onFormSubmit = this.onFormSubmit.bind(this);
   }
 
+  onChange = (event, { newValue, method }) => {
+    this.setState({
+      value: newValue
+    });
+    console.log(this.state);
+  };
+
+  onSuggestionsFetchRequested = ({ value }) => {
+    this.setState({
+      suggestions: getSuggestions(value)
+    });
+  };
+
+  onSuggestionsClearRequested = () => {
+    this.setState({
+      suggestions: []
+    });
+  };
+
   render() {
+    const { value, suggestions } = this.state;
+    const inputProps = {
+      placeholder: "Search projects by technologies",
+      value,
+      onChange: this.onChange
+    };
+
     return (
       <div className = "search-bar col-lg-8 col-lg-offset-2">
         <Form onSubmit = { this.onFormSubmit }>
           <FormGroup className = "search-form">
-            <Input
-              placeholder = "Search projects by technologies"
-              value = { this.state.term }
-              onChange = { event => this.setState({ term: event.target.value }) } />
+            <AutoSuggest
+              suggestions={suggestions}
+              onSuggestionsFetchRequested={this.onSuggestionsFetchRequested}
+              onSuggestionsClearRequested={this.onSuggestionsClearRequested}
+              getSuggestionValue={getSuggestionValue}
+              renderSuggestion={renderSuggestion}
+              inputProps={inputProps} />
             <Button color="primary">
               <i className="fa fa-search" aria-hidden="true"></i>
             </Button>
@@ -28,7 +111,7 @@ class SearchBar extends Component {
 
   onFormSubmit(event) {
     event.preventDefault();
-    this.props.onSearchTermChange(this.state.term);
+    this.props.onSearchTermChange(this.state.value);
   }
 
 }
