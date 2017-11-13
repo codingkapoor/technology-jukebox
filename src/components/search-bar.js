5f76535a30071a4e09ea844aeb40a0f5d9199557
codingkapoor <mailtoshivamk@gmail.com>
2017-11-13 17:33:13 +0530

    builds technologies searchlist

--- src/components/search-bar.js
+++ src/components/search-bar.js
@@ -2,55 +2,7 @@ import React, { Component } from 'react';
 import { Form, FormGroup, Input, Button } from 'reactstrap';
 import AutoSuggest from 'react-autosuggest';
 
-const languages = [
-  {name: "Akka"},
-  {name: "BootStrap"},
-  {name: "CSS3"},
-  {name: "ES6"},
-  {name: "Grunt"},
-  {name: "HTML5"},
-  {name: "Java"},
-  {name: "JavaScript"},
-  {name: "JQuery"},
-  {name: "NPM"},
-  {name: "React"},
-  {name: "Redux"},
-  {name: "Resteasy"},
-  {name: "RequireJS"},
-  {name: "SASS"},
-  {name: "Scala"},
-  {name: "SigmaJS"},
-  {name: "Spring"},
-  {name: "Spring Boot"}
-];
-
-function escapeRegexCharacters(str) {
-  return str.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
-}
-
-function getSuggestions(value) {
-  const escapedValue = escapeRegexCharacters(value.trim());
-
-  if (escapedValue === '') {
-    return [];
-  }
-
-  const regex = new RegExp('^' + escapedValue, 'i');
-
-  return languages.filter(language => regex.test(language.name));
-}
-
-function getSuggestionValue(suggestion) {
-  return suggestion.name;
-}
-
-function renderSuggestion(suggestion) {
-  return (
-    <span>{suggestion.name}</span>
-  );
-}
-
-class SearchBar extends Component {
+export default class SearchBar extends Component {
   constructor(props) {
     super(props);
 
@@ -66,13 +18,11 @@ class SearchBar extends Component {
     this.setState({
       value: newValue
     });
-    console.log(this.state);
   };
 
   onSuggestionsFetchRequested = ({ value }) => {
-    this.setState({
-      suggestions: getSuggestions(value)
-    });
+    let suggestions = getSuggestions(value, this.props.technologies);
+    this.setState({ suggestions });
   };
 
   onSuggestionsClearRequested = () => {
@@ -116,4 +66,28 @@ class SearchBar extends Component {
 
 }
 
-export default SearchBar;
+function escapeRegexCharacters(str) {
+  return str.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
+}
+
+function getSuggestions(value, technologies) {
+  const escapedValue = escapeRegexCharacters(value.trim());
+
+  if (escapedValue === '') {
+    return [];
+  }
+
+  const regex = new RegExp('^' + escapedValue, 'i');
+
+  return technologies.filter(technology => regex.test(technology.name));
+}
+
+function getSuggestionValue(suggestion) {
+  return suggestion.name;
+}
+
+function renderSuggestion(suggestion) {
+  return (
+    <span>{suggestion.name}</span>
+  );
+}
