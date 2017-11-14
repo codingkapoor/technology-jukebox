4f2d5d0c924b731f0571b6fc9c6026833d866264
codingkapoor <mailtoshivamk@gmail.com>
2017-11-14 22:05:54 +0530

    pagination init

--- src/components/app.js
+++ src/components/app.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import _ from 'lodash';
+import UltimatePagination from 'react-ultimate-pagination-bootstrap-4';
 
 import Projects from '../../projects.json';
 
@@ -11,7 +12,16 @@ export default class App extends Component {
     super(props);
 
     this.technologiesSearchPool = getTechnologiesSearchPool();
-    this.state = { projects: Projects };
+    this.state = {
+      projects: Projects,
+      currentPage: 1,
+      totalPages: 2,
+      boundaryPagesRange: 1,
+      siblingPagesRange: 1,
+      hidePreviousAndNextPageLinks: false,
+      hideFirstAndLastPageLinks: true,
+      hideEllipsis: false
+    };
   }
 
   projectSearch(term) {
@@ -26,12 +36,29 @@ export default class App extends Component {
     });
   }
 
+  onPageChangeFromPagination(newPage) {
+    this.setState({ currentPage: newPage });
+    console.log(this.state.currentPage);
+  }
+
   render() {
     return (
       <div>
         <h3 className = "jukebox-header col-lg-12">Technology Jukebox</h3>
         <SearchBar onSearchTermChange = { this.projectSearch.bind(this) } technologiesSearchPool = { this.technologiesSearchPool } />
         <ProjectList projects = { this.state.projects } />
+        <div className = "col-lg-offset-5">
+          <UltimatePagination
+            currentPage = { this.state.currentPage }
+            totalPages = { this.state.totalPages }
+            boundaryPagesRange = { this.state.boundaryPagesRange }
+            siblingPagesRange = { this.state.siblingPagesRange }
+            hidePreviousAndNextPageLinks = { this.state.hidePreviousAndNextPageLinks }
+            hideFirstAndLastPageLinks = { this.state.hideFirstAndLastPageLinks }
+            hideEllipsis = { this.state.hideEllipsis }
+            onChange = { this.onPageChangeFromPagination.bind(this) }
+          />
+        </div>
       </div>
     );
   }
